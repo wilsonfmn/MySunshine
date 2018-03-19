9982da83d4e645b934b01647f6ba5cb67de1d9d9
Wilson Martins Neto <wilsonfmn@gmail.com>
2018-03-19 20:47:41 -0300

    Adding URL connection - Not tested

--- app/src/main/java/com/example/android/mysunshine/MainActivity.java
+++ app/src/main/java/com/example/android/mysunshine/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.mysunshine;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +14,12 @@ import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -61,17 +68,79 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
+            // Com inflate eu consigo montar a hierarquia de layouts
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-            List<String> fakeData = new ArrayList<>();
-            fakeData.add("Today-Sunny-88/63");
-            fakeData.add("Tomorrow-Foggy-70/46");
-            fakeData.add("Weds-Cloudy-72/63");
-            fakeData.add("Thurs-Rainy-64/51");
-            fakeData.add("Fri-Foggy-70/46");
-            fakeData.add("Sat-Sunny-76/68");
+            //List<String> fakeData = new ArrayList<>();
+            //fakeData.add("Today-Sunny-88/63");
+            //fakeData.add("Tomorrow-Foggy-70/46");
+            //fakeData.add("Weds-Cloudy-72/63");
+            //fakeData.add("Thurs-Rainy-64/51");
+            //fakeData.add("Fri-Foggy-70/46");
+            //fakeData.add("Sat-Sunny-76/68");
 
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=05588000,br&mode=json&units=metric&cnt=7&appid=b06972a13aa30297eb847b93d248836d");
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+
+            // Criando a lista de item
             ListAdapter listAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, fakeData);
+            // Consigo pegar diretamente do rootView, tornando a busca menor
             ListView forecastView = rootView.findViewById(R.id.listview_forecast);
             forecastView.setAdapter(listAdapter);
 
